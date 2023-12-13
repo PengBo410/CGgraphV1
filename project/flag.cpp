ecc1d62bfbff7efd243203a2dbf0bb70bd8141b4
pjCui <you@example.com>
2023-12-13 21:24:27 +0800

    TR

--- project/flag.cpp
+++ project/flag.cpp
@@ -11,6 +11,6 @@ DEFINE_int32(computeEngine, SCI32(ComputeEngine_type::COOPERATION), ComputeEngin
 DEFINE_int32(gpuMemory, SCI32(GPU_memory_type::GPU_MEM), GPU_memory_type_help);
 DEFINE_int32(gpuImple, SCI32(GPU_implement_type::COALESCE_CHUNK), GPU_implement_type_help);
 DEFINE_int32(runs, 1, "The Number Of Times The Algorithm Needs To Run");
-DEFINE_bool(has_csrFile, true, "Is There A Processed CSR Binary File Present (Improve The Graph File Loading)");
-DEFINE_bool(logResult, true, "Whether to check the processing results (Need Provided Check File)");
+// DEFINE_bool(has_csrFile, true, "Is There A Processed CSR Binary File Present (Improve The Graph File Loading)");
+// DEFINE_bool(logResult, true, "Whether to check the processing results (Need Provided Check File)");
 DEFINE_int32(useDeviceId, 0, "The GPU ID To Be Used");
